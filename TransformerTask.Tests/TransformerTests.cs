b0114510e428c20d7a791ed830287edfef4c0310
Anzhelika Kravchuk <anzhelika.kravchuk@gmail.com>
2020-05-14 15:11:31 +0000

    add-tests-file

---
+++ TransformerTask.Tests/TransformerTests.cs
@@ -0,0 +1,10 @@
+using NUnit.Framework;
+
+namespace TransformerTask.Tests
+{
+    [TestFixture]
+    public class TransformerTests
+    {
+        //TODO: Add tests for class Transformer.
+    }
+}
